78f81dc3715b9b5dba7fcf4602779b0fc329ae98
Michael Nairn <mnairn@redhat.com>
2020-05-07 17:20:46 +0100

    Add WriteK8sObjectToYAML util, removes k8s runtime managed fileds from object before writing to file.

--- pkg/utils/io_test.go
+++ pkg/utils/io_test.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
 	"path"
@@ -12,6 +13,12 @@ type sampleYAML struct {
 	Value  int            `yaml:"value,omitempty"`
 	Array  []int          `yaml:"array,omitempty"`
 	Object map[string]int `yaml:"object,omitempty"`
+}
+
+type sampleK8sYAML struct {
+	Name              string `yaml:"name,omitempty"`
+	Status            string `yaml:"status"`
+	CreationTimestamp string `yaml:"creationTimestamp"`
 }
 
 func TestPopulateObjectFromYAML(t *testing.T) {
@@ -66,6 +73,33 @@ Object:
   key: 1
 Value: 1
 `
+	if content != expected {
+		t.Errorf("expected output is not valid. Expected:\n%s\n Actual:\n%s\n", expected, content)
+	}
+}
+
+func TestWriteK8sObjectToYAML(t *testing.T) {
+	tmp, err := ioutil.TempDir(os.TempDir(), "test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(tmp)
+	obj := &sampleK8sYAML{
+		Name:              "test",
+		Status:            "test",
+		CreationTimestamp: "test",
+	}
+	dest := path.Join(tmp, "out.yaml")
+	err = WriteK8sObjectToYAML(obj, dest)
+	if err != nil {
+		t.Errorf("failed to write yaml file: %v", err)
+	}
+	content := readFile(t, dest)
+	expected := "name: test\n"
+
+	fmt.Println(content)
+	fmt.Println(expected)
+
 	if content != expected {
 		t.Errorf("expected output is not valid. Expected:\n%s\n Actual:\n%s\n", expected, content)
 	}
