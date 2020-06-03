f3f1c322dc761bdafab8276ccc82a9c385ae342d
Wei Li <wei.li@redhat.com>
2020-06-03 16:45:05 +0100

    add support for serviceAffecting flag when creating a new release

--- cmd/createRelease_test.go
+++ cmd/createRelease_test.go
@@ -36,6 +36,43 @@ func (m mockGitPushService) Push(gitRepo *git.Repository, opts *git.PushOptions)
 	panic("implement me")
 }
 
+func newTestCreateReleaseCmd(serviceAffecting bool) *createReleaseCmd {
+	version, _ := utils.NewRHMIVersion("2.0.0-rc1")
+	cloneService := &mockGitCloneService{cloneToTmpDirFunc: func(prefix string, url string, reference plumbing.ReferenceName) (s string, repository *git.Repository, err error) {
+		currentDir, err := os.Getwd()
+		if err != nil {
+			return "", nil, err
+		}
+		return initRepoFromTestDir("test-create-release-", path.Join(currentDir, "testdata/createReleaseTest"))
+	}}
+	pushService := &mockGitPushService{pushFunc: func(gitRepo *git.Repository, opts *git.PushOptions) error {
+		return nil
+	}}
+	prService := mockPullRequestsService{
+		ListFunc: func(ctx context.Context, owner string, repo string, opts *github.PullRequestListOptions) (requests []*github.PullRequest, response *github.Response, err error) {
+			return []*github.PullRequest{}, nil, nil
+		},
+		CreateFunc: func(ctx context.Context, owner string, repo string, pull *github.NewPullRequest) (request *github.PullRequest, response *github.Response, err error) {
+			url := "http://testurl"
+			return &github.PullRequest{
+				HTMLURL: &url,
+			}, nil, nil
+		},
+	}
+	return &createReleaseCmd{
+		version:          version,
+		repoInfo:         &githubRepoInfo{owner: "test", repo: "test"},
+		baseBranch:       "master",
+		releaseScript:    "release.sh",
+		gitUser:          "testuser",
+		gitPass:          "testpass",
+		gitCloneService:  cloneService,
+		gitPushService:   pushService,
+		githubPRService:  prService,
+		serviceAffecting: serviceAffecting,
+	}
+}
+
 func TestCreateRelease(t *testing.T) {
 	cases := []struct {
 		description string
@@ -44,41 +81,9 @@ func TestCreateRelease(t *testing.T) {
 		verify      func(repoDir string) error
 	}{
 		{
-			description: "should finish successfully",
+			description: "should finish successfully when serviceAffecting is true",
 			cmd: func() *createReleaseCmd {
-				version, _ := utils.NewRHMIVersion("2.0.0-rc1")
-				cloneService := &mockGitCloneService{cloneToTmpDirFunc: func(prefix string, url string, reference plumbing.ReferenceName) (s string, repository *git.Repository, err error) {
-					currentDir, err := os.Getwd()
-					if err != nil {
-						return "", nil, err
-					}
-					return initRepoFromTestDir("test-create-release-", path.Join(currentDir, "testdata/createReleaseTest"))
-				}}
-				pushService := &mockGitPushService{pushFunc: func(gitRepo *git.Repository, opts *git.PushOptions) error {
-					return nil
-				}}
-				prService := mockPullRequestsService{
-					ListFunc: func(ctx context.Context, owner string, repo string, opts *github.PullRequestListOptions) (requests []*github.PullRequest, response *github.Response, err error) {
-						return []*github.PullRequest{}, nil, nil
-					},
-					CreateFunc: func(ctx context.Context, owner string, repo string, pull *github.NewPullRequest) (request *github.PullRequest, response *github.Response, err error) {
-						url := "http://testurl"
-						return &github.PullRequest{
-							HTMLURL: &url,
-						}, nil, nil
-					},
-				}
-				return &createReleaseCmd{
-					version:         version,
-					repoInfo:        &githubRepoInfo{owner: "test", repo: "test"},
-					baseBranch:      "master",
-					releaseScript:   "release.sh",
-					gitUser:         "testuser",
-					gitPass:         "testpass",
-					gitCloneService: cloneService,
-					gitPushService:  pushService,
-					githubPRService: prService,
-				}
+				return newTestCreateReleaseCmd(true)
 			},
 			expectError: false,
 			verify: func(repoDir string) error {
@@ -103,6 +108,44 @@ func TestCreateRelease(t *testing.T) {
 				}
 				if strings.Index(string(content), "2.0.0-rc1") < 0 {
 					return fmt.Errorf("expected: %s, but got %s", "2.0.0-rc1", string(content))
+				}
+				if strings.Index(string(content), "ServiceAffecting=false") >= 0 {
+					return fmt.Errorf("unexpected output: ServiceAffecting=false in output: %s", string(content))
+				}
+				return nil
+			},
+		},
+		{
+			description: "should finish successfully when serviceAffecting is false",
+			cmd: func() *createReleaseCmd {
+				return newTestCreateReleaseCmd(false)
+			},
+			expectError: false,
+			verify: func(repoDir string) error {
+				repo, err := git.PlainOpen(repoDir)
+				if err != nil {
+					return err
+				}
+				repoTree, err := repo.Worktree()
+				if err != nil {
+					return err
+				}
+				status, err := repoTree.Status()
+				if err != nil {
+					return err
+				}
+				if len(status) > 0 {
+					return fmt.Errorf("there are uncommited changes in the repo: %v", status)
+				}
+				content, err := ioutil.ReadFile(path.Join(repoDir, "VERSION.txt"))
+				if err != nil {
+					return err
+				}
+				if strings.Index(string(content), "2.0.0-rc1") < 0 {
+					return fmt.Errorf("expected: %s, but got %s", "2.0.0-rc1", string(content))
+				}
+				if strings.Index(string(content), "ServiceAffecting=false") < 0 {
+					return fmt.Errorf("expecting ServiceAffecting=false in output: %s", string(content))
 				}
 				return nil
 			},
--- cmd/testdata/createReleaseTest/release.sh
+++ cmd/testdata/createReleaseTest/release.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
 echo $SEMVER > VERSION.txt
+if [[ ! -z "$NONE_SERVICE_AFFECTING" ]]; then
+ echo "ServiceAffecting=false" >> VERSION.txt
+fi
