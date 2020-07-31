13a0f9dc71c2b7c6e2710422908fe73b4be64144
stobin <stobin@redhat.com>
2020-07-31 12:17:35 +0100

    add set-product-version command

---
+++ cmd/manageRHMITypes.go
@@ -0,0 +1,126 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"golang.org/x/mod/semver"
+	"io/ioutil"
+	"os"
+	"regexp"
+	"strings"
+)
+
+type manageTypesCmdOptions struct {
+	directory string
+	product   string
+	version   string
+}
+
+func init() {
+
+	flags := &manageTypesCmdOptions{}
+
+	cmd := &cobra.Command{
+		Use:   "set-product-version",
+		Short: "Sets the operator and product version for a product in the rhmi_types file",
+		Run: func(cmd *cobra.Command, args []string) {
+			err := SetVersion(flags.directory, flags.product, flags.version)
+			if err != nil {
+				handleError(err)
+			}
+		},
+	}
+
+	ewsCmd.AddCommand(cmd)
+
+	cmd.Flags().StringVarP(&flags.directory, "directory", "d", "", "Path to rhmi_types file")
+	cmd.MarkFlagRequired("directory")
+
+	cmd.Flags().StringVarP(&flags.product, "product", "p", "", "The product name")
+	cmd.MarkFlagRequired("product")
+
+	cmd.Flags().StringVarP(&flags.version, "version", "v", "", "The desired version")
+	cmd.MarkFlagRequired("version")
+}
+
+func SetVersion(directory string, product string, version string) error {
+	product = PrepareProductName(product)
+	fmt.Println(fmt.Sprintf("setting version of product %s to %s", product, version))
+	read, err := os.Open(directory)
+	if err != nil {
+		return err
+	}
+	bytes, err := ioutil.ReadAll(read)
+	if err != nil {
+		return err
+	}
+	out := ParseVersion(bytes, product, version)
+	fmt.Println(fmt.Sprintf("writing changes to rhmi_types file at %s ", directory))
+	err = ioutil.WriteFile(directory, []byte(out), 600)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func PrepareProductName(product string) string {
+	switch product {
+	case "3scale":
+		product = "3Scale"
+	case "amq-online":
+		product = "AMQOnline"
+	case "amq-streams":
+		product = "AMQStreams"
+	case "apicurito":
+		product = "Apicurito"
+	case "codeready-workspaces":
+		product = "CodeReadyWorkspaces"
+	case "fuse-online":
+		product = "FuseOnline"
+	case "rhsso":
+		product = "RHSSO"
+	}
+
+	return product
+}
+
+func ParseVersion(in []byte, product string, version string) string {
+	// remove whitespace and "'s
+	version = strings.ReplaceAll(version, "\"", "")
+	version = strings.TrimSpace(version)
+
+	var ReOperatorVersion = regexp.MustCompile(`OperatorVersion` + product + `.*`)
+	var ReProductVersion = regexp.MustCompile(`Version` + product + `.*`)
+
+	operatorVersion := ReOperatorVersion.FindString(string(in))
+	productVersion := ReProductVersion.FindString(string(in))
+
+	// remove whitespace and "'s
+	ovs := strings.Split(operatorVersion, "=")[1]
+	ovs = strings.ReplaceAll(ovs, "\"", "")
+	ovs = strings.TrimSpace(ovs)
+
+	pvs := strings.Split(productVersion, "=")[1]
+
+	currentOperatorMajor := semver.Major("v" + ovs)
+	newOperatorMajor := semver.Major("v" + version)
+
+	var out string
+	// we only want to set the Version<product> var to CHANGEME for minor releases
+	fmt.Println(fmt.Sprintf("current OperatorVersion: %s Found OperatorVersion: %s", currentOperatorMajor, newOperatorMajor))
+	c := semver.Compare(currentOperatorMajor, newOperatorMajor)
+	// major versions are a match
+	if c == 0 {
+		out = strings.Replace(string(in), pvs, " \"CHANGEME\"", 1)
+		r := strings.Replace(operatorVersion, ovs, version, 1)
+		out = strings.Replace(out, operatorVersion, r, 1)
+	}
+
+	// if it's not a minor release only change the OperatorVersion<product> var
+	// if the version is 0.x.x then semver returns empty string so account for it
+	if c != 0 || currentOperatorMajor == "" && newOperatorMajor == "" {
+		out = strings.Replace(string(in), ovs, version, 1)
+	}
+
+	return out
+}
---
+++ cmd/manageRHMITypes_test.go
@@ -0,0 +1,157 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"github.com/integr8ly/delorean/pkg/utils"
+	"io/ioutil"
+	"os"
+	"path"
+	"regexp"
+	"strings"
+	"testing"
+)
+
+func TestManageRHMITypes(t *testing.T) {
+	type args struct {
+		ctx     context.Context
+		cmdOpts *manageTypesCmdOptions
+	}
+	tests := []struct {
+		name    string
+		args    args
+		verify  func(t *testing.T, directory string, product string, version string, majmin string) error
+		wantErr bool
+		majmin  string
+	}{
+		{
+			name: "test setting 3scale major version",
+			args: args{context.TODO(), &manageTypesCmdOptions{
+				directory: "",
+				product:   "3scale",
+				version:   "9.9.9",
+			}},
+			wantErr: false,
+			verify: func(t *testing.T, directory string, product string, version string, majmin string) error {
+				return verifyTypesFile(t, directory, product, version, "")
+			},
+			majmin: "major",
+		},
+		{
+			name: "test setting amq-online major version",
+			args: args{context.TODO(), &manageTypesCmdOptions{
+				directory: "",
+				product:   "amq-online",
+				version:   "9.9.9",
+			}},
+			wantErr: false,
+			verify: func(t *testing.T, directory string, product string, version string, majmin string) error {
+				return verifyTypesFile(t, directory, product, version, "")
+			},
+			majmin: "major",
+		},
+		{
+			name: "test setting 3scale minor version",
+			args: args{context.TODO(), &manageTypesCmdOptions{
+				directory: "",
+				product:   "3scale",
+				version:   "9.10.0",
+			}},
+			wantErr: false,
+			verify: func(t *testing.T, directory string, product string, version string, majmin string) error {
+				return verifyTypesFile(t, directory, product, version, "")
+			},
+			majmin: "minor",
+		},
+		{
+			name: "test setting amq-online minor version",
+			args: args{context.TODO(), &manageTypesCmdOptions{
+				directory: "",
+				product:   "amq-online",
+				version:   "9.10.0",
+			}},
+			wantErr: false,
+			verify: func(t *testing.T, directory string, product string, version string, majmin string) error {
+				return verifyTypesFile(t, directory, product, version, "")
+			},
+			majmin: "minor",
+		},
+	}
+	for _, tt := range tests {
+		testDir, err := ioutil.TempDir(os.TempDir(), "test-")
+		if err != nil {
+			t.Fatal(err)
+		}
+		err = utils.CopyDirectory("./testdata/manageRHMITypes", testDir)
+		if err != nil {
+			t.Fatal(err)
+		}
+		tt.args.cmdOpts.directory = path.Join(testDir, "rhmi_types")
+		t.Run(tt.name, func(t *testing.T) {
+			if err := SetVersion(tt.args.cmdOpts.directory, tt.args.cmdOpts.product, tt.args.cmdOpts.version); (err != nil) != tt.wantErr {
+				t.Errorf("SetVersion() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			if !tt.wantErr {
+				if tt.verify != nil {
+					if err := tt.verify(t, tt.args.cmdOpts.directory, tt.args.cmdOpts.product, tt.args.cmdOpts.version, tt.majmin); err != nil {
+						fmt.Println("d: ", tt.args.cmdOpts.directory)
+						t.Fatalf("verification failed due to error: %v", err)
+					}
+				}
+			}
+
+			err = os.RemoveAll(testDir)
+			if err != nil {
+				t.Fatal(err)
+			}
+		})
+	}
+}
+
+func verifyTypesFile(t *testing.T, directory, product string, version string, majmin string) (error) {
+	f, err := os.Open(directory)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	bytes, _ := ioutil.ReadAll(f)
+	product = PrepareProductName(product)
+
+	var ReOperatorVersion = regexp.MustCompile(`OperatorVersion` + product + `.*`)
+	var ReProductVersion = regexp.MustCompile(`Version` + product + `.*`)
+
+	operatorVersion := ReOperatorVersion.FindString(string(bytes))
+	productVersion := ReProductVersion.FindString(string(bytes))
+
+	// Remove the "'s so it can validate against the version
+	ovs := strings.Split(operatorVersion, "=")[1]
+	ovs = strings.ReplaceAll(ovs, "\"", "")
+	ovs = strings.TrimSpace(ovs)
+
+	pvs := strings.Split(productVersion, "=")[1]
+	pvs = strings.ReplaceAll(pvs, "\"", "")
+	pvs = strings.TrimSpace(pvs)
+
+	if majmin == "major" {
+		if ovs != version {
+			t.Errorf("error found incorrect version string, wanted = %s, got %s", ovs, version)
+		}
+		if pvs == "CHANGEME" {
+			t.Errorf("error found incorrect version string got %s", pvs)
+		}
+	}
+
+	if majmin == "minor" {
+		if ovs != version {
+			t.Errorf("error found incorrect version string, wanted = %s, got %s", ovs, version)
+		}
+		if pvs != "CHANGEME" {
+			t.Errorf("error found incorrect version string, wanted = CHANGEME got %s", pvs)
+		}
+	}
+
+
+	return nil
+}
