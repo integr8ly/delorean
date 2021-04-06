4f1c3dbf965fedf9e85649469cecd9d094d12f6a
MStokluska <mstoklus@redhat.com>
2021-04-06 17:08:36 +0100

    manifest generation update

--- cmd/testdata/createProdsecManifestTest/prodsec-manifest-generator.sh
+++ cmd/testdata/createProdsecManifestTest/prodsec-manifest-generator.sh
@@ -1,14 +1,36 @@
 #!/usr/bin/env bash
 
 # Dependencies used
-
+case $TYPE_OF_MANIFEST in 
+  "production")
+  RHMIfileName="rhmi-production-release-manifest.txt"
+  RHOAMfileName="rhoam-production-release-manifest.txt"
+  ;;
+  "master")
+  RHMIfileName="rhmi-master-manifest.txt"
+  RHOAMfileName="rhoam-master-manifest.txt"
+  ;;
+  "compare")
+    case $OLM_TYPE in
+      "integreatly-operator")
+        exit 0
+        ;;
+      "managed-api-service")
+        exit 1
+        ;;
+       *)
+        ;;
+      esac
+  ;;
+  *)
+esac
 
 case $OLM_TYPE in
   "integreatly-operator")
-    echo "dummy manifest dependency" > "prodsec-manifests/rhmi-production-release-manifest.txt"
+    echo "dummy manifest dependency" > "prodsec-manifests/$RHMIfileName"
     ;;
   "managed-api-service")
-    echo "dummy manifest dependency" > "prodsec-manifests/rhoam-production-release-manifest.txt"
+    echo "dummy manifest dependency" > "prodsec-manifests/$RHOAMfileName"
     ;;
    *)
     echo "Invalid OLM_TYPE set"
