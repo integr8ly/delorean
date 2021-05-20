a247467dcdd83336cb5e2bf9c4f30d9c3996e0fe
Pavel Šturc <psturc@redhat.com>
2021-05-20 12:02:22 +0200

    fix: id -> uuid in rp/types_test.go

--- pkg/reportportal/types_test.go
+++ pkg/reportportal/types_test.go
@@ -4,28 +4,28 @@ import "testing"
 
 func TestRPLaunchResponse_GetLaunchUuid(t *testing.T) {
 	cases := []struct {
-		description string
-		msg         string
-		expectedId  string
+		description  string
+		msg          string
+		expectedUuid string
 	}{
 		{
-			description: "success without quote",
-			msg:         "Launch with id = b862b3c3-a9ce-47d1-9f5c-e51ae9de50f3 is successfully imported",
-			expectedId:  "b862b3c3-a9ce-47d1-9f5c-e51ae9de50f3",
+			description:  "success without quote",
+			msg:          "Launch with id = b862b3c3-a9ce-47d1-9f5c-e51ae9de50f3 is successfully imported",
+			expectedUuid: "b862b3c3-a9ce-47d1-9f5c-e51ae9de50f3",
 		},
 		{
-			description: "success with quote",
-			msg:         "Launch with ID = 'b862b3c3-a9ce-47d1-9f5c-e51ae9de50f3' successfully updated",
-			expectedId:  "b862b3c3-a9ce-47d1-9f5c-e51ae9de50f3",
+			description:  "success with quote",
+			msg:          "Launch with ID = 'b862b3c3-a9ce-47d1-9f5c-e51ae9de50f3' successfully updated",
+			expectedUuid: "b862b3c3-a9ce-47d1-9f5c-e51ae9de50f3",
 		},
 	}
 
 	for _, c := range cases {
 		t.Run(c.description, func(t *testing.T) {
 			r := RPLaunchResponse{Message: c.msg}
-			actualId := r.GetLaunchUuid()
-			if actualId != c.expectedId {
-				t.Fatalf("expected id: %s got: %s", c.expectedId, actualId)
+			actualUuid := r.GetLaunchUuid()
+			if actualUuid != c.expectedUuid {
+				t.Fatalf("expected id: %s got: %s", c.expectedUuid, actualUuid)
 			}
 		})
 	}
